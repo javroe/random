28c7d00bac0e481326c1d1b3a1fc4b900c53f730
javroe <140004823+javroe@users.noreply.github.com>
2024-06-20 15:43:23 +0300

    Add files via upload

---
+++ troll_warning.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def troll_message():
+    tk.messagebox.showwarning(title="grrrr", message="ur gunna regret not giving me that computer name...")
+
+troll_message()
