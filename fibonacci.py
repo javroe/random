ba9dfbd2103f068c10fbbef5a8aedf05672f5845
javroe <bjayroe02@gmail.com>
2024-06-24 17:53:26 +0300

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+#each number is equal to the sum of the preceding two numbers
+
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2 
+count = 1
+
+while count <= n:
+	print(next_number, end=" ")
+	count += 1
+	num1, num2 = num2, next_number
+	next_number = num1 + num2
+print()
+
+    
