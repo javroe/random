4930202d003cf4e81aec2514412b46a141258716
javroe <bjayroe02@gmail.com>
2024-06-27 18:18:25 +0300

    made a dumb random number generator game

---
+++ random_number_game.py
@@ -0,0 +1,25 @@
+import random
+
+magic_number = random.randrange(11)
+count = 0
+guess = -1
+
+while True:
+    count += 1
+    guess = int(input("Guess the magic number: "))
+
+    if guess == magic_number:
+        break
+    else:
+        if guess > magic_number:
+            print("Try guessing lower")
+        else:
+            print("Try guessing higher")
+        
+
+if count == 1:
+    print("Congrats! You got it on the first try!")
+else:
+    print("Congrats! It only took you " + str(count) + " tries!")
+
+print("The magic number was " + str(magic_number))
